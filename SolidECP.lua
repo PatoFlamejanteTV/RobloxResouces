64914d10dd4b579dd874bc9ea43a070cf39fc423
UltimateQuack <patoflamejantetv@gmail.com>
2024-09-10 14:27:57 -0300

    Create SolidECP.lua

---
+++ SolidECP.lua
@@ -0,0 +1,9 @@
+--[[
+From the first versions of Solid10,
+the emergency code plan system has been implemented,
+bringing updated updates in real time (in the client-side(eg.: bugfixes)
+]]
+
+print("[SolidECP]: Loaded Lua ECP!")
+-- ...
+print("[SolidECP]: Ended Lua ECP!")
