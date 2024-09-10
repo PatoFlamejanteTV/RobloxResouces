71ff7b23308c4ecdda59b8b2ed7f383cbdd97d33
UltimateQuack <patoflamejantetv@gmail.com>
2024-09-10 14:34:25 -0300

    Update SolidECP.lua

--- SolidECP.lua
+++ SolidECP.lua
@@ -5,5 +5,7 @@ bringing updated updates in real time (in the client-side(eg.: bugfixes)
 ]]
 
 print("[SolidECP]: Loaded Lua ECP!")
--- ...
+
+print("[SolidECP]: Testing, 1... 2... 3...!")
+
 print("[SolidECP]: Ended Lua ECP!")
